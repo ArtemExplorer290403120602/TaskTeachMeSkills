a9014ea114aee83b32d6927b8da4fe51a3922495
User <artem.zakrzhevski@mail.ru>
2023-10-10 03:50:30 +0300

    добавил сюда несколько переменных))

--- src/GitTask.java
+++ src/GitTask.java
@@ -2,6 +2,10 @@ public class GitTask {
     public static void main(String[] args) {
         System.out.println("TeachMeSkills!");
         int a=4;
+        int b=5;
+        int c=45;
+        int f;
+        f=a+b+c;
 
     }
 }
